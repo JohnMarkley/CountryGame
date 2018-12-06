0943676851719cfa789980b36e57ef93879d8598
JohnMarkley <johnwilliammarkley@gmail.com>
2018-12-07 06:45:29 +0800

    Updates

---
+++ precache-manifest.bc8b776790e8e411eef84fb9f8055958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51df750194588a184f3c",
+    "url": "/CountryGame/static/js/runtime~main.51df7501.js"
+  },
+  {
+    "revision": "0ca9b3dfe43471fe3222",
+    "url": "/CountryGame/static/js/main.0ca9b3df.chunk.js"
+  },
+  {
+    "revision": "cd2e4bb4c1d9d9dee9a1",
+    "url": "/CountryGame/static/js/1.cd2e4bb4.chunk.js"
+  },
+  {
+    "revision": "0ca9b3dfe43471fe3222",
+    "url": "/CountryGame/static/css/main.73843dc4.chunk.css"
+  },
+  {
+    "revision": "cd2e4bb4c1d9d9dee9a1",
+    "url": "/CountryGame/static/css/1.d7bcf7f8.chunk.css"
+  },
+  {
+    "revision": "45318c89f5653eb46da1db6d38f23247",
+    "url": "/CountryGame/index.html"
+  }
+];
